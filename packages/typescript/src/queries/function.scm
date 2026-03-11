e48f17b8e1e45a6020dab1569dfc2e071d57603c
Juun <blacklily7513@gmail.com>
2026-03-11 12:08:49 +0900

    chore(plugin): Add async capturing for both function and method

--- packages/typescript/src/queries/function.scm
+++ packages/typescript/src/queries/function.scm
@@ -1,7 +1,8 @@
 ;; function declaration
-;; function @name<@type_param>(@params): @return_type @body
-;; function* @name<@type_param>(@params): @return_type @body
+;; @is_async function @name@type_params@params: @return_type @body
+;; @is_async function* @name@type_params@params: @return_type @body
 (function_declaration
+  "async"? @is_async
   name: (identifier) @name
   type_parameters: (type_parameters (type_parameter)+ @type_param)? @type_params
   parameters: (formal_parameters) @params
@@ -10,6 +11,7 @@
 )@function
 
 (generator_function_declaration
+  "async"? @is_async
   name: (identifier) @name
   type_parameters: (type_parameters (type_parameter)+ @type_param)?
   parameters: (formal_parameters) @params
@@ -18,18 +20,41 @@
 ) @function
 
 ;; arrow function / function expression
-;; const/let @name = (@params) => @body
-;; const/let @name = function (@params) @body
+;; const/let @name = @is_async @params: @return_type => @body
+;; const/let @name = @is_async function @params: @return_type @body
 (lexical_declaration
   (variable_declarator
     name: (identifier) @name
     value: [
       (arrow_function
+        "async"? @is_async
         type_parameters: (type_parameters (type_parameter)+ @type_param)? @type_params
         parameters: [(formal_parameters) (identifier)] @params
         return_type: (type_annotation (_) @return_type)?
         body: (_) @body)
       (function_expression
+        "async"? @is_async
+        type_parameters: (type_parameters (type_parameter)+ @type_param)? @type_params
+        parameters: (formal_parameters) @params
+        return_type: (type_annotation (_) @return_type)?
+        body: (statement_block) @body)
+    ])
+) @function
+
+;; var @name = @is_async @params: @return_type => @body
+;; var @name = @is_async function @params: @return_type @body
+(variable_declaration
+  (variable_declarator
+    name: (identifier) @name
+    value: [
+      (arrow_function
+        "async"? @is_async
+        type_parameters: (type_parameters (type_parameter)+ @type_param)? @type_params
+        parameters: [(formal_parameters) (identifier)] @params
+        return_type: (type_annotation (_) @return_type)?
+        body: (_) @body)
+      (function_expression
+        "async"? @is_async
         type_parameters: (type_parameters (type_parameter)+ @type_param)? @type_params
         parameters: (formal_parameters) @params
         return_type: (type_annotation (_) @return_type)?
--- packages/typescript/src/queries/method.scm
+++ packages/typescript/src/queries/method.scm
@@ -1,8 +1,9 @@
 ;; methods
-;; @modifier @is_static @name<@type_params>(@params): @return_type @body
+;; @modifier @is_static @is_async @name@type_params@params: @return_type @body
 (method_definition
   [(accessibility_modifier) (override_modifier)]? @modifier
-  ("static")? @is_static
+  "static"? @is_static
+  "async"? @is_async
   name: (property_identifier) @name
   type_parameters: (type_parameters)? @type_params
   parameters: (formal_parameters) @params
@@ -12,19 +13,21 @@
 ) @method
 
 ;; arrow function / function expression methods
-;; @modifier @is_static @name = <@type_params>(@params): @return_type => @body
-;; @modifier @is_static @name = function <@type_params>(@params) @body
+;; @modifier @is_static @name = @is_async @type_params@params: @return_type => @body
+;; @modifier @is_static @name = @is_async function @type_params@params @body
 (public_field_definition
   [(accessibility_modifier) (override_modifier)]? @modifier
-  ("static")? @is_static
+  "static"? @is_static
   name: (property_identifier) @name
   value: [
     (arrow_function
+      "async"? @is_async
       type_parameters: (type_parameters)? @type_params
       parameters: [(formal_parameters) (identifier)] @params
       return_type: (type_annotation (_) @return_type)?
       body: (_) @body)
     (function_expression
+      "async"? @is_async
       type_parameters: (type_parameters)? @type_params
       parameters: (formal_parameters) @params
       return_type: (type_annotation (_) @return_type)?
